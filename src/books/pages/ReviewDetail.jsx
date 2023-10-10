91927e203a44980774c02011302a1e5a4fb870f2
Christopher <valdebenitochristopher@gmail.com>
2023-10-10 17:31:16 -0300

    socket de likes

--- src/books/pages/ReviewDetail.jsx
+++ src/books/pages/ReviewDetail.jsx
@@ -3,16 +3,39 @@ import { BackButton, Loader, StarRatingWithoutChange } from '../components'
 
 import { useSelector } from 'react-redux'
 import { useReviewStore } from '../../hooks'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import styles from '../styles/ReviewDetail.module.css'
+import booksApi from '../../api/booksApi'
 
 export const ReviewDetail = () => {
   const { reviewList, isLoadingReview } = useSelector(state => state.review)
+  const { user } = useSelector(state => state.auth)
+  const [likes, setLikes] = useState(0)
   const { id } = useParams()
   const { startLoadingReviews } = useReviewStore()
 
   const review = reviewList.find(review => review.id === Number(id))
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'))
+
+    const config = {
+      headers: {
+        Authorization: `Token ${token}`
+      }
+    }
+    const getLikes = async () => {
+      try {
+        const likes = await booksApi.get(`api/reviews/reviews_likes/${id}/`, config)
+        const data = likes.data
+        setLikes(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getLikes()
+  }, [])
 
   useEffect(() => {
     startLoadingReviews()
@@ -24,6 +47,35 @@ export const ReviewDetail = () => {
       <Loader />
       </div>
     )
+  }
+
+  //* websocket
+  const handleLike = () => {
+    const dataToSend = {
+      action: 'like',
+      id: user.id,
+      review_id: id
+    }
+    const websocket = new WebSocket('ws://localhost:8000/LB_API/consumer/likes')
+
+    websocket.onopen = () => {
+      websocket.send(JSON.stringify(dataToSend))
+    }
+
+    websocket.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      console.log('Received data from server:', data)
+      setLikes(data.likes)
+      websocket.close()
+    }
+
+    websocket.onerror = (error) => {
+      console.error('WebSocket error:', error)
+    }
+
+    websocket.onclose = () => {
+      console.log('WebSocket closed')
+    }
   }
   const fullName = `${review.user.first_name} ${review.user.last_name}`
 
@@ -72,8 +124,8 @@ export const ReviewDetail = () => {
           <p className={styles.description}>{review.description}</p>
 
           <div className={styles.likeButton}>
-          <button>Like</button>
-          <span>10 likes</span>
+          <button onClick={handleLike}>Like</button>
+          <span>{likes.likes} likes</span>
           </div>
         </div>
 
