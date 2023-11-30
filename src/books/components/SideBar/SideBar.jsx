ebcb04eb7da88b4aa9754e394e8359595ab51574
Christopher <valdebenitochristopher@gmail.com>
2023-11-29 22:13:59 -0300

    notificacion hecha

--- src/books/components/SideBar/SideBar.jsx
+++ src/books/components/SideBar/SideBar.jsx
@@ -3,60 +3,45 @@ import { NavBar, AccountButton } from '../'
 import { linksNav } from '../../services/linksArray'
 
 import styles from './SideBar.module.css'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import toast, { Toaster } from 'react-hot-toast'
 import { useSelector } from 'react-redux'
 
 export const Sidebar = ({ handleModal, modalOpen }) => {
   const { user } = useSelector(state => state.auth)
-  const [wsNotification, setWsNotification] = useState(null)
-  // console.log(wsNotification)
 
   const NavOpen = useNavOpen()
 
   useEffect(() => {
-    const connectWebSocket = () => {
-      const socket = new WebSocket(`ws://localhost:8000/ws/notifications/${user.id}/`)
+    const socket = new WebSocket(`ws://localhost:8000/ws/notifications/${user.id}/`)
 
-      socket.onopen = (event) => {
-        console.log('WebSocket conectado')
-      }
-
-      socket.onclose = (event) => {
-        console.log('WebSocket desconectado. Intentando reconectar en 5 segundos...')
-        setTimeout(() => {
-          connectWebSocket() // Intentar reconectar después de 5 segundos
-        }, 5000)
-      }
-
-      socket.onmessage = (msg) => {
-        try {
-          const dataServer = JSON.parse(msg.data.message)
-          console.log(dataServer)
-          toast(
-            dataServer.message,
-            {
-              duration: 6000
-            }
-          )
-        } catch (error) {
-          console.error('Error al procesar WebSocket:', error)
-        }
-      }
-
-      // setWsNotification(socket)
+    socket.onopen = (event) => {
+      console.log('WebSocket conectado')
     }
 
-    // Iniciar la conexión al montar el componente
-    connectWebSocket()
+    socket.onclose = (event) => {
+      console.log('WebSocket desconectado.')
+    }
 
-    // Limpieza al desmontar el componente
-    return () => {
-      if (wsNotification) {
-        wsNotification.close()
+    socket.onmessage = async (msg) => {
+      try {
+        const dataServer = JSON.parse(msg.data)
+        const message = JSON.parse(dataServer.message)
+        console.log(message)
+        toast(
+          message.message,
+          {
+            duration: 6000
+          }
+
+        )
+      } catch (error) {
+        console.error('Error al procesar WebSocket:', error)
       }
     }
-  }, [])
+  }
+
+  , [])
 
   return (
   <>
